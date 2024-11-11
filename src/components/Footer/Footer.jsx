b317484ce362c1385b00adfeeeaf65f951441ab0
ericsato77 <bsc-inf-me-03-21@unima.ac.mw>
2024-11-11 14:59:21 +0200

    Created Footer Component and contents

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div></div>
-  )
+    <div className="footer">
+      <p>dhis2 patient_reminders 2024</p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
