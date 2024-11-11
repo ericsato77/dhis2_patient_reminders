4b60d411e26555321b69ccd51c0ffc7f80684792
ericsato77 <bsc-inf-me-03-21@unima.ac.mw>
2024-11-11 16:01:13 +0200

    Added copyright symbol to the footer copyright statement

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <p>dhis2 patient_reminders 2024</p>
+      <p>&#169; dhis2 patient_reminders 2024</p>
     </div>
   );
 }
