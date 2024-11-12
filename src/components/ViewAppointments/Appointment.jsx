0f743fb898c0be67708dc73c6a51e5e0d6e8ccaf
Kennedy Soko <bsc-inf-16-21@unima.ac.mw>
2024-11-12 13:30:52 +0200

    creating HTML table

--- src/components/ViewAppointments/Appointment.jsx
+++ src/components/ViewAppointments/Appointment.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 function Appointment() {
   return (
-    <div>Appointment</div>
-  )
+    <div className="appointments-container">
+      <div className="header">
+       
+      </div>
+
+      <h2>Appointments</h2>
+
+      <table className="appointments-table">
+        <thead>
+          <tr>
+            <th>Patient Name</th>
+            <th>Appointment Date & Time</th>
+            <th>Reminder Status</th>
+            <th>Type of Appointment</th>
+            <th>Location</th>
+            <th>Contact Information</th>
+            <th>Notes</th>
+            <th>Health Worker Assigned</th>
+          </tr>
+        </thead>
+        
+      </table>
+    </div>
+  );
 }
 
-export default Appointment
+export default Appointment;
